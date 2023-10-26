638f49a043749ce8bf3ebf245a2db67da524b18d
Mico Tongco <89470974+MicoT@users.noreply.github.com>
2023-10-26 11:08:33 +0800

    Delete server directory

--- server/routes/logincourse.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getUser } from "../controllers/logincourse.js";
-
-const router = express.Router();
-
-router.get("/user/:id", getUser);
-export default  router;
--- server/routes/overview.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getStudents } from "../controllers/overview";
-
-const router = express.Router();
-
-router.get("/student/:id", getStudents);
-export default  router;
